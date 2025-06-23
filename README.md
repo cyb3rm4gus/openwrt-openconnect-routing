89b146f41376febf77a2c6a9a686dfe2179d396a
cyb3rm4gus <cybersec78@gmail.com>
2025-06-23 17:29:17 +0700

    fix

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # openwrt-openconnect-routing
 
 
-
 sh -c "$(wget -qO- https://raw.githubusercontent.com/cyb3rm4gus/openwrt-openconnect-routing/refs/heads/main/configure.sh)"
--- configure.sh
+++ configure.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 set -e
 
-read -p "Enter VPN username: " VPN_USER
-stty -echo
-read -p "Enter VPN password: " VPN_PASS
-stty echo
-echo
-read -p "Enter VPN server URL (e.g. vpn.example.com/?secret): " VPN_URL
+printf "Enter VPN username: "
+read -r VPN_USER
+
+printf "Enter VPN password: "
+read -r VPN_PASS
+
+printf "Enter VPN server URL (e.g. vpn.example.com/?secret): "
+read -r VPN_URL
 
 VPN_DOMAIN=$(echo "$VPN_URL" | cut -d/ -f1)
 VPN_IP=$(nslookup "$VPN_DOMAIN" | awk '/^Address: / { print $2 }' | tail -n1)
-[ -z "$VPN_IP" ] && echo "Failed to resolve VPN IP" && exit 1
+if [ -z "$VPN_IP" ]; then
+  echo "Failed to resolve VPN IP"
+  exit 1
+fi
 
-# Setup OpenConnect interface
 uci set network.oc0="interface"
 uci set network.oc0.proto="openconnect"
 uci set network.oc0.username="$VPN_USER"
@@ -22,58 +26,48 @@ uci set network.oc0.defaultroute="1"
 uci set network.oc0.auto="1"
 uci commit network
 
-# Add firewall zone "vpn"
 uci add firewall zone
-ZONE_INDEX=$(uci get firewall.@zone[-1].index)
-uci set firewall.@zone[-1].name="vpn"
-uci set firewall.@zone[-1].input="REJECT"
-uci set firewall.@zone[-1].output="ACCEPT"
-uci set firewall.@zone[-1].forward="REJECT"
-uci add_list firewall.@zone[-1].network="oc0"
+ZONE_ID=$(uci add firewall zone)
+uci set firewall.$ZONE_ID.name="vpn"
+uci set firewall.$ZONE_ID.input="REJECT"
+uci set firewall.$ZONE_ID.output="ACCEPT"
+uci set firewall.$ZONE_ID.forward="REJECT"
+uci add_list firewall.$ZONE_ID.network="oc0"
 
-# Deny forwarding by default
 uci set firewall.@defaults[0].forward="REJECT"
 
-# Add forwarding lan -> vpn
-uci add firewall forwarding
-FORWARD_INDEX=$(uci get firewall.@forwarding[-1].index)
-uci set firewall.@forwarding[-1].src="lan"
-uci set firewall.@forwarding[-1].dest="vpn"
+FORWARD_ID=$(uci add firewall forwarding)
+uci set firewall.$FORWARD_ID.src="lan"
+uci set firewall.$FORWARD_ID.dest="vpn"
 
-# Add firewall rule to block WAN DNS
-uci add firewall rule
-RULE_INDEX=$(uci get firewall.@rule[-1].index)
-uci set firewall.@rule[-1].name="Block WAN DNS"
-uci set firewall.@rule[-1].src="lan"
-uci set firewall.@rule[-1].dest="wan"
-uci set firewall.@rule[-1].dest_port="53"
-uci set firewall.@rule[-1].proto="tcp udp"
-uci set firewall.@rule[-1].target="REJECT"
+RULE_ID=$(uci add firewall rule)
+uci set firewall.$RULE_ID.name="Block WAN DNS"
+uci set firewall.$RULE_ID.src="lan"
+uci set firewall.$RULE_ID.dest="wan"
+uci set firewall.$RULE_ID.dest_port="53"
+uci set firewall.$RULE_ID.proto="tcp udp"
+uci set firewall.$RULE_ID.target="REJECT"
 
-# Add redirect DNS to VPN-safe DNS (1.1.1.1)
-uci add firewall redirect
-REDIRECT_INDEX=$(uci get firewall.@redirect[-1].index)
-uci set firewall.@redirect[-1].name="Force LAN DNS to VPN-safe DNS"
-uci set firewall.@redirect[-1].src="lan"
-uci set firewall.@redirect[-1].proto="tcp udp"
-uci set firewall.@redirect[-1].src_dport="53"
-uci set firewall.@redirect[-1].dest_ip="1.1.1.1"
-uci set firewall.@redirect[-1].dest_port="53"
-uci set firewall.@redirect[-1].target="DNAT"
+REDIRECT_ID=$(uci add firewall redirect)
+uci set firewall.$REDIRECT_ID.name="Force LAN DNS to VPN-safe DNS"
+uci set firewall.$REDIRECT_ID.src="lan"
+uci set firewall.$REDIRECT_ID.proto="tcp udp"
+uci set firewall.$REDIRECT_ID.src_dport="53"
+uci set firewall.$REDIRECT_ID.dest_ip="1.1.1.1"
+uci set firewall.$REDIRECT_ID.dest_port="53"
+uci set firewall.$REDIRECT_ID.target="DNAT"
 
-# Configure dnsmasq
 uci set dhcp.@dnsmasq[0].noresolv="1"
 uci del_list dhcp.@dnsmasq[0].server
 uci add_list dhcp.@dnsmasq[0].server="1.1.1.1"
 uci add_list dhcp.@dnsmasq[0].server="9.9.9.9"
 
-# Commit changes
 uci commit firewall
 uci commit dhcp
+
 /etc/init.d/firewall restart
 /etc/init.d/dnsmasq restart
 
-# Hotplug script to store WAN route info
 cat << 'EOF' > /etc/hotplug.d/iface/10-store-wan-route
 #!/bin/sh
 [ "$ACTION" = "ifup" ] || exit 0
@@ -87,7 +81,6 @@ DEV=$(ubus call network.interface.$INTERFACE status | jsonfilter -e '@.l3_device
 EOF
 chmod +x /etc/hotplug.d/iface/10-store-wan-route
 
-# Hotplug script to set static route to VPN server
 cat << EOF > /etc/hotplug.d/iface/99-vpn-route
 #!/bin/sh
 LOGFILE="/tmp/hp.txt"
@@ -116,4 +109,4 @@ echo "Set route to \$VPN_IP via \$GW dev \$DEV" >> \$LOGFILE
 EOF
 chmod +x /etc/hotplug.d/iface/99-vpn-route
 
-echo "Setup complete. Reboot router to activate VPN."
+echo "VPN setup complete. Reboot router to apply all changes."
